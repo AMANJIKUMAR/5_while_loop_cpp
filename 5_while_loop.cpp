9562f6133e73453c1f81f8558a9e67f07f75efd1
Aman kumar <72993406+AMANJIKUMAR@users.noreply.github.com>
2022-03-01 18:14:39 +0530

    Add files via upload

---
+++ 5_while_loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+int n;
+cin>>n;
+int i=1;
+while (i<=n)    // intilization
+{
+cout<<i<<' ';    //task
+i=i+1 ;          //updation
+}
+cout<<endl;
+return 0;
+}
+
+
+
+
+//print no till enter (we use cin for input)
